5358eb6f8b24f88ded0f010e7a0eb32c218226f4
f.pugliese <f.pugliese@reply.it>
2020-11-24 13:25:25 +0100

    Propagate changes from father branch to son

---
+++ devops/pipelines/propagate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+MAIN_DIR="${SCRIPT_DIR}/../../"
+
+SONS_FILE="${MAIN_DIR}/devops/branches/sons.txt"
+
+# -----
+
+# Get current branch
+current=$( git branch | grep '*' | cut -f 2 -d ' ' )
+
+# Get its sons
+sons=$( cat ${SONS_FILE} | grep "${current}:" | cut -f 2 -d ':' )
+
+# For each son check for merge conflicts
+for son in ${sons}; do
+
+    echo "Merging into ${son}"
+    git checkout ${son}
+    git merge --no-edit --no-ff ${son}
+
+done
+
+# If this is reached, there were no conflicts: push all the merges.
+git push --all
