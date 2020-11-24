482470e3cf8e6827339cdb80d20455f785f32f92
f.pugliese <f.pugliese@reply.it>
2020-11-24 17:51:02 +0100

    Using bitbucket pip caching and env variable

--- devops/pipelines/test.sh
+++ devops/pipelines/test.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 MAIN_DIR="${SCRIPT_DIR}/../.."
 
 
-BRANCH=$( git branch | grep '*' | cut -f 2 -d ' ' )
+BRANCH=${BITBUCKET_BRANCH}
 UNIT_TESTS_DIR="${SCRIPT_DIR}/../tests/unit/${BRANCH}"
 
 pip install pytest==6.1.2
