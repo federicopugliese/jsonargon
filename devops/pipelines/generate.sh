095f5f8de2c2f3c180544cf7d8301f92e968e870
f.pugliese <f.pugliese@reply.it>
2021-04-21 10:07:26 +0200

    Generating additional module pipelines

---
+++ devops/pipelines/generate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+MAIN_DIR="${SCRIPT_DIR}/../.."
+
+# Move to the main folder
+cd ${MAIN_DIR}
+
+# Fill the generate.yml
+echo ${GENERATE_YML_BASE64} | base64 -d > generate.yml
+
+# Generate
+python generate.py --local-generation
