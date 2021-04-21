793c84324658f342d945fd0c3ae7155bbfa6c172
f.pugliese <f.pugliese@reply.it>
2021-04-21 10:11:49 +0200

    Fetch all the branches

--- devops/pipelines/generate.sh
+++ devops/pipelines/generate.sh
@@ -4,6 +4,9 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 MAIN_DIR="${SCRIPT_DIR}/../.."
 
+# Fetch all the branches
+git fetch origin 'refs/heads/*:refs/remotes/origin/*'
+
 # Move to the main folder
 cd ${MAIN_DIR}
 
