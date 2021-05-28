dad18ba0b7b6f2cdd78bbaab67068dcc74294025
f.pugliese <f.pugliese@reply.it>
2021-05-28 19:38:14 +0200

    Licence and refs

--- src/jsonargon/package/setup.py
+++ src/jsonargon/package/setup.py
@@ -25,5 +25,15 @@ setup(
     description="Serialization and deserialization of JSON objects from/into Python objects (with validation and remapping capabilities)",
     packages=find_packages(),
     package_data={name: _get_resources(name), "tests": ["stubs/*"]},
-    install_requires=requirements
+    install_requires=requirements,
+
+    url="https://gitlab.com/federico_pugliese/jsonargon",
+    author="Federico Pugliese",
+    license="Apache Software License, Version 2.0",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7"
+    ]
 )
